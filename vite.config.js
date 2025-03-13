6a477eda0aa40a9ecdccf40f6b10a871a8c6fdba
eugenegraves <elg22@njit.edu>
2025-03-12 23:42:25 -0400

    GitHub Pages Deployment Attempt #10

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/The-Effects-of-Diet-on-Obesity/',
   build: {
     outDir: 'dist'
   }
